a1f6957b4522c8a3a2696a3d548c5edef24b303b
Courage <couragesblog@gmail.com>
2023-02-28 19:36:34 +0100

    Add increment and decrement functions

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,20 @@
 import { ref, computed } from 'vue'
 const count = ref(0)
 const counter = computed(() => `Current count (${count.value}) + 1 = ${count.value + 1}`)
+
+const increment = () => count.value++
+
+const decrement = () => {
+  if (count.value == 0) return;
+  count.value--
+}
 </script>
 
 <template>
   <div>
     <h1>{{ counter }}</h1>
-    <button @click="count++">increment</button>
+    <button @click="increment">increment</button>
 
-    <button @click="count--">decrement</button>
+    <button @click="decrement">decrement</button>
   </div>
 </template>
