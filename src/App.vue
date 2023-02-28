a14112179bc1246735370e5c0e5d970bb7d6515f
Courage <couragesblog@gmail.com>
2023-02-28 20:21:52 +0100

    Move logic into useConter composable

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-const count = ref(0)
-const counter = computed(() => `Current count (${count.value}) + 1 = ${count.value + 1}`)
-
-const increment = () => count.value++
-
-const decrement = () => {
-  if (count.value == 0) return;
-  count.value--
-}
+import { useCounter } from './composables/useCounter'
+const { counter, increment, decrement } = useCounter()
 </script>
 
 <template>
