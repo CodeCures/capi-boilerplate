884d301a4bc107dee3ddaaed1a564c5d00cd0ce2
Courage <couragesblog@gmail.com>
2023-02-28 17:50:59 +0100

    Add reactive counter variable
    - increment
    - decrement

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const counter = ref(0)
+</script>
+
 <template>
   <div>
-    <h1>Boiler plate</h1>
-    <p>Hello boilerplate</p>
+    <h1>{{ counter }}</h1>
+    <button @click="counter++">increment</button>
+
+    <button @click="counter--">decrement</button>
   </div>
 </template>
